4c358f3288a21963a00b47c64a6799257611464b
JinhaSong <thejn07@gmail.com>
2020-03-29 21:43:34 +0900

    Fixed Bug

--- WebAnalyzer/serializers.py
+++ WebAnalyzer/serializers.py
@@ -6,5 +6,5 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = ImageModel
-        fields = ('image', 'token', 'uploaded_date', 'updated_date', 'results')
-        read_only_fields = ('token', 'uploaded_date', 'updated_date', 'results')
+        fields = ('image', 'token', 'uploaded_date', 'updated_date', 'result')
+        read_only_fields = ('token', 'uploaded_date', 'updated_date', 'result')
